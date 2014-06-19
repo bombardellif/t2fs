ae080cd32711a528766a63f03a42609bd65bf39c
Fernando Bombardelli <bombardelli.f@gmail.com>
2014-06-18 22:51:11 -0300

    Implmentation of the FilePath module

--- include/FilePath.h
+++ include/FilePath.h
@@ -1,13 +1,23 @@
 #ifndef FILE_PATH_H
 #define FILE_PATH_H
 
+#include "t2fs.h"
+
+#define DS '/'
+
 typedef struct s_FilePath {
 	char* path;
+    char* pathWithoutLastNode;
+    BOOL parsed;
+    char* currentToken;
+    BOOL tokenWasRetrieved;
 } FilePath;
 
-void FilePath(FilePath* this, char* path);
-FilePath withoutLastNode(FilePath* this);
-char* getNextNode(FilePath* this);
-BOOL hasNextNode(FilePath* this);
+void FP_FilePath(FilePath* this, char* path);
+void FP_destroy(FilePath* this);
+void FP_withoutLastNode(FilePath* this, FilePath* new);
+char* FP_getLastNode(FilePath* this);
+char* FP_getNextNode(FilePath* this);
+BOOL FP_hasNextNode(FilePath* this);
 
 #endif
--- src/FilePath.c
+++ src/FilePath.c
@@ -1,21 +1,100 @@
 #include "FilePath.h"
 
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include "t2fs.h"
 
 void FP_FilePath(FilePath* this, char* path)
 {
+    this->pathWithoutLastNode = this->path = this->currentToken = NULL;
+    this->parsed = FALSE;
+    this->tokenWasRetrieved = TRUE;
+    
+    if (path) {
+
+        int pathLen = strlen(path);
+        this->path = (char*) calloc(pathLen + 1, sizeof(char));
+        strcpy(this->path, path);
+
+
+        // if the last char of the path is a directory separator, then, erase it
+        if (this->path[pathLen-1] == DS) {
+            this->path[pathLen-1] = '\0';
+        }
+
+        // Set the path wothout last node:
+        char* lastDelim = strrchr(this->path, DS);
+        if (lastDelim) {
+            int pathUntilDelimSize = lastDelim - this->path + 1;
+            this->pathWithoutLastNode = (char*) calloc(pathUntilDelimSize, sizeof(char));
+
+            // copy from path to pathWithoutLastNode until the last delimiter
+            lastDelim* = '\0';
+            strcpy(this->pathWithoutLastNode, this->path);
+            lastDelim* = DS;
+        } else {
+            this->pathWithoutLastNode = NULL;
+        }
+    }
 }
 
-FilePath FP_withoutLastNode(FilePath* this)
+void FP_destroy(FilePath* this)
 {
-	return 0;
+    if (this) {
+        if (this->path) {
+            free(this->path);
+            this->path = NULL;
+        }
+        if (this->pathWithoutLastNode) {
+            free(this->pathWithoutLastNode);
+            this->pathWithoutLastNode = NULL;
+        }
+    }
+}
+
+void FP_withoutLastNode(FilePath* this, FilePath* new)
+{
+    // Create new File Path without the last node of this File Path
+	FP_FilePath(new, this->pathWithoutLastNode);
+}
+
+static char* FP__nextNode(FilePath* this)
+{
+    if (!this->path) {
+        return NULL;
+    } else if (this->parsed) {
+        return this->currentToken = strtok(NULL, DS);
+    } else {
+        this->parsed = TRUE;
+        return this->currentToken = strtok(this->path, DS);
+    }
 }
 
 char* FP_getNextNode(FilePath* this)
 {
-	return 0;
+    if (this->tokenWasRetrieved) {
+        return FP__nextNode(this);
+    } else {
+        this->tokenWasRetrieved = TRUE;
+        return this->currentToken;
+    }
 }
 
-boolean FP_hasNextNode(FilePath* this)
+BOOL FP_hasNextNode(FilePath* this)
 {
-	return 0;
+    // if the currentToken was already retrieved, then tokenize again for the
+    // next token in the path
+    if (this->tokenWasRetrieved) {
+        
+        // this call will set this->currentToken
+        FP__nextNode(this);
+        this->tokenWasRetrieved = FALSE;
+    }
+    
+    // if this->currentToken is not NULL, then there is a next node in the path
+    if (this->currentToken)
+        return TRUE;
+    else
+        return FALSE;
 }
