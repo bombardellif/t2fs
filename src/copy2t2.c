fdfe5cac90f329254d87b510252358d340e8db98
Fernando Bombardelli <bombardelli.f@gmail.com>
2014-06-30 22:07:19 -0300

    Development of utilities

---
+++ src/copy2t2.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   copy2t2.c
+ * Author: fernando
+ *
+ * Created on 30. Juni 2014, 22:03
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
